13880361813092c035b4275d7b75b3db9b32ef1b
bsachinthana <bsachinthana@gmail.com>
2018-08-31 23:52:16 +0530

    Home only

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  myForm:FormGroup;
 
+
+  constructor(public navCtrl: NavController,private fb:FormBuilder) {
+    this.myForm = fb.group({
+      rad: ['rust'],
+      name:['',Validators.required]
+    });
+  }
+  setType(vehicle){
+    this.myForm.controls['rad'].setValue(vehicle)
   }
 
 }
